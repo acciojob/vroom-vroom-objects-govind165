283f0b7a844b3989bdbe71404babff4b7399a7af
govind165 <mrgovindsarbes@gmail.com>
2026-07-19 21:21:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
 
-function SportsCar(make, model, topSpeed) {}
+
+function SportsCar(make, model, topSpeed) {
+	 Car.call(this, make, model);
+	 this.topSpeed = topSpeed;
+}
+
 
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
+
+	const car = new Car("Toyota", "Corolla");
+console.log(car.make);      
+console.log(car.model);     
+
+const sportsCar = new SportsCar("Ferrari", "F8", 340);
+console.log(sportsCar.make);      
+console.log(sportsCar.model);     
+console.log(sportsCar.topSpeed);  
