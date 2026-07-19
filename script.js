09867e17f26c47f120c99d8dd3da6f2b45506502
govind165 <mrgovindsarbes@gmail.com>
2026-07-19 21:24:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,11 +14,3 @@ function SportsCar(make, model, topSpeed) {
 window.Car = Car;
 window.SportsCar = SportsCar;
 
-	const car = new Car("Toyota", "Corolla");
-console.log(car.make);      
-console.log(car.model);     
-
-const sportsCar = new SportsCar("Ferrari", "F8", 340);
-console.log(sportsCar.make);      
-console.log(sportsCar.model);     
-console.log(sportsCar.topSpeed);  
